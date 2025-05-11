17cb0dc94478ce62d4975c33748958c125cf7dd8
minindu-alwis <minindualwis@gmail.com>
2025-05-11 11:58:37 +0530

    feat: add ConvexClientProvider and integrate user creation logic; update RootLayout and Provider components for user state management

--- app/provider.js
+++ app/provider.js
@@ -5,17 +5,30 @@ import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '@/configs/fireBaseConfig';
 import AuthContext from './_context/AuthContext';
+import { useMutation } from 'convex/react';
+import { api } from '@/convex/_generated/api';
 
 function Provider({ children }) {
   const [user, setUser] = useState();
 
+  const CreateUser = useMutation(api.users.CreateNewUser);
+
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
       console.log(user, "check user");
       setUser(user);
+
+     
+          const result = await CreateUser({
+            name: user.displayName,
+            email: user.email,
+            pictureURL: user.photoURL,
+          });
+          console.log(result, "check result");
     });
+
     return () => unsubscribe();
-  }, []);
+  }, [CreateUser]);
 
   return (
     <AuthContext.Provider value={{ user }}>
---
+++ convex/users.js
@@ -0,0 +1,26 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const CreateNewUser = mutation({
+    args:{
+        name:v.string(),
+        email:v.string(),
+        pictureURL:v.string(),
+    },
+    handler:async(convexToJson,args) => {
+        const user=await convexToJson.db.query('users')
+        .filter((q)=>q.eq(q.field('email'),args.email))
+        .collect();
+
+        if(!user[0]?.email){
+            const result=await convexToJson.db.insert('users',{
+                name:args.name,
+                email:args.email,
+                pictureURL:args.pictureURL,
+                credits:3
+            })
+            return result;
+        }
+        return user[0];
+    }
+})
