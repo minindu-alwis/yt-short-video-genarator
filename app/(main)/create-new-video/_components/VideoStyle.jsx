db04005c64aa10ec506bffc241c5c5e5596ce9e2
minindu-alwis <minindualwis@gmail.com>
2025-05-19 09:37:48 +0530

    feat: enhance VideoStyle component with state management for selected style and improve logo SVG with enhanced gradients and animations

--- app/(main)/create-new-video/_components/VideoStyle.jsx
+++ app/(main)/create-new-video/_components/VideoStyle.jsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 
 const options=[
     {
@@ -29,19 +29,28 @@ const options=[
 ]
 function VideoStyle({onHandleInputChanges}) {
 
+    const [selectedStyle,setSelectedStyle]=useState();
+
   return (
-    <div>
+    <div className='mt-5'>
         <h2>Video Style</h2>
         <p className='text-sm text-gray-400 mb-1'>Select Video Style</p>
 
         <div className='grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-2'>
             {options?.map((option,index)=>(
-               <div className='relative'>
+               <div className='relative'
+               onClick={()=>{setSelectedStyle(option.name);
+                onHandleInputChanges('imageStyle',option.name)
+               }}
+               >
                     <Image src={option.image}
                     alt={option.name}
                     width={500}
                     height={120}
-                    className='object-cover h-[70px] lg:h[130px] xl:h-[180px] rounded-lg p-1 hover:border border-gray-400 cursor-pointer'
+                    className={`object-cover h-[70px] lg:h[130px]
+                     xl:h-[180px] rounded-lg p-1 hover:border
+                      border-gray-400 cursor-pointer
+                       ${option.name == selectedStyle && 'border'}`}
                     />
                     <h2 className='absolute bottom-1 text-center w-full'>{option.name}</h2>
                </div>
