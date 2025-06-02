95aa15b92b87ffdc991011e58b3aecde7f610bec
minindu-alwis <minindualwis@gmail.com>
2025-06-02 22:10:19 +0530

    feat: remove unused GenarateImageScript and related code from functions.js

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { inngest } from "./client";
 import { createClient } from "@deepgram/sdk";
-import { GenarateImageScript } from "@/configs/AiModel";
 
 export const helloWorld = inngest.createFunction(
     { id: "hello-world" },
@@ -11,16 +10,6 @@ export const helloWorld = inngest.createFunction(
         return { message: `Hello ${event.data.email}!` };
     },
 );
-
-const ImagePromtScript = `Genarate Image promt of (style) style
-with all details for each scene for 30 seconds video : script : {script}
-- Just Give specifing image prompt depends on story line 
-- don not give camera angle image promt
--follow the following schema and return JSON data (MAX 4-5 Images)
--[{
-    imagePrompt:'',
-    sceneContetnt: '<Script Content>'
-}]`
 
 
 const BASE_URL = 'https://aigurulab.tech';
@@ -53,48 +42,36 @@ export const GenarateVideoData = inngest.createFunction(
             });
 
         //Genarate Captions
-        // const GenaratedCaptions = await step.run(
-        //     "genarateCaptions",
-        //     async () => {
-        //         const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY);
+        const GenaratedCaptions = await step.run(
+            "genarateCaptions",
+            async () => {
+                const deepgram = createClient(process.env.NEXT_PUBLIC_DEEPGRAM_API_KEY);
 
-        //         const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
-        //             {
-        //                 url: GenarateAudioFile,
-        //             },
-        //             // STEP 3: Configure Deepgram options for audio analysis
-        //             {
-        //                 model: "nova-3",
-        //                 smart_format: true,
-        //             }
-        //         );
-        //         return result.results?.channels[0]?.alternatives[0]?.words;
-        //     }
+                const { result, error } = await deepgram.listen.prerecorded.transcribeUrl(
+                    {
+                        url: GenarateAudioFile,
+                    },
+                    // STEP 3: Configure Deepgram options for audio analysis
+                    {
+                        model: "nova-3",
+                        smart_format: true,
+                    }
+                );
+                return result.results?.channels[0]?.alternatives[0]?.words;
+            }
 
 
-        // )
+        )
 
         //Genarate Image Promt From  Script
-        const GenaratedImagePromts = await step.run(
-            "geanarateImagePromt",
-            async () =>{
-                const FINAL_PROMPT=ImagePromtScript.replace
-                ('{style}',videoStyle).replace('script',script);
-                const result=await GenarateImageScript.sendMessage(FINAL_PROMPT);
-                const resp=JSON.parse(result.response.text());
-
-                return resp;
-            }
-        )
 
         //Gwnarate Images using ai
 
         //Save All To database
 
-        return GenaratedImagePromts;
+        return GenaratedCaptions;
     }
 )
-
 
 
 
