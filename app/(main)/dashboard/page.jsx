3b99cb0377043f28542036848124c84d55d4a413
minindu-alwis <minindualwis@gmail.com>
2025-06-04 07:30:58 +0530

    feat: implement Dashboard component with VideoList integration

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import VideoList from './_components/VideoList'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>
+      <h2 className='font-bold text-3xl'>My Videos</h2>
+      <VideoList />
+    </div>
   )
 }
 
