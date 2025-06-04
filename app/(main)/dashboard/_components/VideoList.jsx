a068fd50f7e863580ae7d8e7c05cb7146ff5549e
minindu-alwis <minindualwis@gmail.com>
2025-06-04 08:21:29 +0530

    feat: enhance VideoList component to display message and button when no videos are available

--- app/(main)/dashboard/_components/VideoList.jsx
+++ app/(main)/dashboard/_components/VideoList.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+"use client";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useState } from 'react'
 
 function VideoList() {
+    const [videoList , setVideoList] = useState([]);
+
+    const getUserVideoList = () =>{
+        
+    }
+
   return (
-    <div>VideoList</div>
+    <div>
+        {videoList?.length==0 && 
+        <div className='flex flex-col items-center justify-center mt-28 gap-5 p-5 border-dashed rounded-xl py-16' >
+            <Image src={'/logo.svg'} alt='logo' width={150} height={150}/>
+            <h2 className='text-gray-400 text-lg'>You Dont Have Any Video Created !</h2>
+            <Link href={'/create-new-video'}>
+            <Button>+ Create New Video</Button>
+            </Link>
+        </div>
+        }
+    </div>
   )
 }
 
