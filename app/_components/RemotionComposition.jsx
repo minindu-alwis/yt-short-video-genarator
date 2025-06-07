ea2c4dee3a687f6ed15cd693abe65af293e17303
minindu-alwis <minindualwis@gmail.com>
2025-06-07 20:55:19 +0530

    feat: enhance RemotionComposition component to include image rendering and dynamic duration calculation

--- app/_components/RemotionComposition.jsx
+++ app/_components/RemotionComposition.jsx
@@ -1,9 +1,15 @@
 import React, { useEffect } from 'react'
-import { useVideoConfig } from 'remotion';
+import { AbsoluteFill, Img, Sequence, useVideoConfig } from 'remotion';
 
 function RemotionComposition({videoData,setDurationInFrame}) {
+    
     const captions=videoData?.cationJson;
     const {fps}=useVideoConfig();
+    const imageList=videoData?.images;
+    console.log("videoData",videoData);
+    console.log("captions",captions);
+    console.log("imageList",imageList);
+    
 
     useEffect(() => {
       videoData && getDurationFrame();
@@ -12,10 +18,36 @@ function RemotionComposition({videoData,setDurationInFrame}) {
     const getDurationFrame=()=>{
       const totalDuration=captions[captions?.length-1]?.end * fps;
       setDurationInFrame(totalDuration);
+      return totalDuration;
     }
 
   return (
-    <div>RemotionComposition</div>
+    <div>
+      <AbsoluteFill>
+
+
+      {imageList?.map((item,index)=>{
+        const startTime=(index*getDurationFrame())/imageList?.length;
+        const duration=getDurationFrame();
+        return(
+          <>
+          <Sequence key={index} from={startTime} durationInFrames={getDurationFrame()}>
+            <AbsoluteFill>
+              <Img
+              src={item}
+              style={{
+                width: '100%',
+                height: '100%',
+                objectFit: 'cover',
+              }}
+              />
+            </AbsoluteFill>
+          </Sequence>
+          </>
+        )
+      })}
+      </AbsoluteFill>
+    </div>
   )
 }
 
