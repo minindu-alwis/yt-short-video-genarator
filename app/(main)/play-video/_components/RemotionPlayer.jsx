ae22548a8d561f45dd3125e87425e03e107582a4
minindu-alwis <minindualwis@gmail.com>
2025-06-07 20:03:01 +0530

    feat: enhance RemotionPlayer and RemotionComposition components to manage video duration dynamically

--- app/(main)/play-video/_components/RemotionPlayer.jsx
+++ app/(main)/play-video/_components/RemotionPlayer.jsx
@@ -1,21 +1,29 @@
 "use client";
 
-import React from 'react'
+import React, { useState } from 'react'
 import {Player} from '@remotion/player';
 import RemotionComposition from '@/app/_components/RemotionComposition';
+import { useVideoConfig } from 'remotion';
 
 function RemotionPlayer({videoData}) {
+
+  const [durationInFrame,setDurationInFrame]=useState(100)
   return (
     <div>
          <Player
       component={RemotionComposition}
-      durationInFrames={120}
+      durationInFrames={Number(durationInFrame.toFixed(0)) + 100}
       compositionWidth={720}
       compositionHeight={1280}
       fps={30}
       controls
+      style={{
+        width:'25vw',
+        height:'70vh'
+      }}
       inputProps={{
-        videoData:videoData
+        videoData:videoData,
+        setDurationInFrame:(frameValue)=>setDurationInFrame(frameValue)
       }}
     />
     </div>
--- app/_components/RemotionComposition.jsx
+++ app/_components/RemotionComposition.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useVideoConfig } from 'remotion';
 
-function RemotionComposition() {
+function RemotionComposition({videoData,setDurationInFrame}) {
+    const captions=videoData?.cationJson;
+    const {fps}=useVideoConfig();
+
+    useEffect(() => {
+      videoData && getDurationFrame();
+    },[videoData])
+
+    const getDurationFrame=()=>{
+      const totalDuration=captions[captions?.length-1]?.end * fps;
+      setDurationInFrame(totalDuration);
+    }
+
   return (
     <div>RemotionComposition</div>
   )
