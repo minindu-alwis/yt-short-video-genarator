f33a8802f6fbba7f4adc98538b595132987b3d0d
minindu-alwis <minindualwis@gmail.com>
2025-06-09 22:28:57 +0530

    fix: update VideoInfo component to use optional chaining and improve layout with new button for export

--- app/(main)/play-video/_components/VideoInfo.jsx
+++ app/(main)/play-video/_components/VideoInfo.jsx
@@ -1,18 +1,27 @@
-import { ArrowLeft } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { ArrowLeft, DownloadIcon } from 'lucide-react'
+import Link from 'next/link'
 import React from 'react'
 
 function VideoInfo({videoData}) {
-  console.log("alwis",videoData)
+  
   return (
-    <div>
+    <div className='flex flex-col gap-3 p-5 border-rounded'>
+      <Link href='/dashboard'>
       <h2 className='flex gap-2 items-center'>
         <ArrowLeft/>
         Back To The Dashboard
       </h2>
+      </Link>
       <h2 className='mt-5'>
         Project Name : {videoData?.title}
       </h2>
-      <p className='text-gray-500'>Script : {videoData.script} </p>
+      <p className='text-gray-500'>Script : {videoData?.script} </p>
+      <h2>Video Style : {videoData?.videoStyle}</h2>
+
+      <Button>
+        <DownloadIcon/>Export & Download
+      </Button>
     </div>
   )
 }
