b1462d8dc63807eaaeded43839cd14cda31939d5
minindu-alwis <minindualwis@gmail.com>
2025-06-17 03:23:01 +0530

    fix: correct spelling of "Generator" in Hero component header

--- app/_components/Hero.jsx
+++ app/_components/Hero.jsx
@@ -5,7 +5,7 @@ import Authentication from './Authentication'
 function Hero() {
   return (
     <div className="p-7 flex flex-col items-center justify-center mt-24 md:px-20 lg:px-36 xl:px-48">
-        <h2 className="font-bold text-6xl text-center">AI Youtube Short Video Genarator</h2>
+        <h2 className="font-bold text-6xl text-center">AI Youtube Short Video Generator</h2>
         <p className='mt-4 text-2xl text-center text-gray-300'>
              🤖 Instantly create engaging YouTube Shorts using the power of AI!⚡
         It generates scripts, images, and voiceovers — all in seconds.
